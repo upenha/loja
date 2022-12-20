4e97db269f973f6080dfdb98b3414c953bbec1f4
Upenha <contato.upenha@gmail.com>
2022-12-20 06:10:20 -0300

    added emojis

--- source/commands/Owner/bot.ts
+++ source/commands/Owner/bot.ts
@@ -18,8 +18,15 @@ export class EvalCommand extends Command {
 	}
 	public async chatInputRun(interaction: Command.ChatInputInteraction) {
 		await interaction.deferReply()
-		await interaction.channel?.send({ embeds: [new MessageEmbed().setImage('https://i.imgur.com/sPbFfbX.png').setColor('#2f3136')]})
-		await interaction.channel?.send({ embeds: [new Embed().setImage('https://i.imgur.com/p112FgS.png').setTitle('<:chat:1054684650041253949> | Deseja criar um ticket?').setDescription('Para criar um ticket clique no botão abaixo!')], components: [new MessageActionRow().addComponents(new MessageButton({ emoji: '1054684654910832730', style: 'SECONDARY', label: 'Abrir ticket', customId: 'createTicket'}))]})
+		await interaction.channel?.send({ embeds: [new MessageEmbed().setImage('https://i.imgur.com/EtFObIf.png').setColor('#2f3136')]})
+		await interaction.channel?.send({ 
+      embeds: [
+        new Embed()
+        .setImage('https://i.imgur.com/p112FgS.png')
+        .setTitle('`💸` | Deseja criar um ticket?')
+        .setDescription('Para criar um ticket clique no botão abaixo!')
+      ]
+    })
 		await interaction.deleteReply()
 	}
 }
